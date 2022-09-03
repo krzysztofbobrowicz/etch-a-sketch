bbb3ef4c30b4f84d48672427c3d80ccba5983a09
Krzysztof Bobrowicz <kbobrowicz98@gmail.com>
2022-09-03 09:54:42 +0200

    Finish basic logic and create functions

--- script.js
+++ script.js
@@ -1,25 +1,58 @@
 const container = document.querySelector('#container');
-const grid = document.querySelector('#grid');
-container.appendChild(grid);
-let col = 1;
-// const gridPrompt = prompt(
-//   'Please choose how many cols would you like to create (choose between 1 and 100)'
-// );
+const chooseSizeButton = document.querySelector('#choose-size');
+const eraser = document.querySelector('#eraser');
+let gridSize = 16;
 
-const gridSize = 16;
-
-while (col <= gridSize) {
-  const colClass = document.createElement('div');
-  colClass.classList.add(`col${col}`);
-  grid.appendChild(colClass);
-  for (let i = 0; i < gridSize; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    square.style.width = 40 / gridSize + 'em';
-    square.style.height = 40 / gridSize + 'em';
-    colClass.appendChild(square);
-
-    square.addEventListener('mouseover', () => square.classList.add('active'));
+chooseSizeButton.addEventListener('click', () => {
+  gridSize = parseInt(
+    prompt(
+      'Please choose how many cols would you like to create (choose between 1 and 100)'
+    )
+  );
+  if (gridSize > 100) {
+    return;
   }
-  col++;
+  grid.remove();
+  createGrid();
+});
+
+function createGrid() {
+  let col = 1;
+  console.log(gridSize);
+  const grid = document.createElement('div');
+  grid.setAttribute('id', 'grid');
+  container.appendChild(grid);
+
+  while (col <= gridSize) {
+    console.log(gridSize);
+    const colClass = document.createElement('div');
+    colClass.classList.add(`col${col}`);
+    grid.appendChild(colClass);
+    for (let i = 0; i < gridSize; i++) {
+      const square = document.createElement('div');
+      square.classList.add('square');
+      square.style.width = 40 / gridSize + 'em';
+      square.style.height = 40 / gridSize + 'em';
+      colClass.appendChild(square);
+      paintOnHover(square);
+      eraseElements(square);
+    }
+    col++;
+  }
 }
+
+function paintOnHover(square) {
+  square.addEventListener('mouseover', () => square.classList.add('active'));
+}
+
+function eraseElements(square) {
+  eraser.addEventListener('click', () =>
+    square.addEventListener('mouseover', () =>
+      square.classList.contains('active')
+        ? square.classList.remove('active')
+        : none
+    )
+  );
+}
+
+createGrid();
